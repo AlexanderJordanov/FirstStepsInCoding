d090d2a6f99869bf53cbd3866dc6df3f039becc6
AlexanderJordanov <tafdokidoki9708@gmail.com>
2023-09-30 15:39:41 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double year = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "m")
+            {
+                if (year >= 16) { Console.WriteLine("Mr."); }
+                else  { Console.WriteLine("Master"); }
+            }
+            else if (gender == "f")
+            {
+                if (year >= 16) { Console.WriteLine("Ms."); }
+                else  { Console.WriteLine("Miss"); }
+            }
+           
+
+
+        }
+    }
+}
